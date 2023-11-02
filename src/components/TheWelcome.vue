eceabbc749bb2b0d17dbc5728b4cc400f661ba84
xuyichenklkkop <352544840@qq.com>
2023-11-03 00:49:10 +0800

    init vue3+vite+pinia

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import DrawingView from '@/views/DrawingView.vue'
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Vue’s
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+    provides you with all information you need to get started.
+  </WelcomeItem>
+
+  <DrawingView />
+</template>
---
+++ src/views/DrawingView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>111aaa</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped></style>
